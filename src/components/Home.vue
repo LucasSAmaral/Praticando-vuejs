c1fe3418e4a679a7bb8884429f9ee2f894dffa15
Amaral <lucas@grito.cc>
2018-07-11 17:11:52 -0300

    Primeiro commit

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="teste">
+        <p>{{ola}}</p>
+        {{numero}}
+        <Somar @click.native="aumenta"></Somar>
+    </div>
+</template>
+
+<script>
+    import $ from 'jquery';
+    import Somar from './Somar';
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                ola: 'SOMANDO...',
+                numero: 0
+            }
+        },
+        methods: {
+            aumenta: function(){
+                this.numero = this.numero + 1;
+                
+            },
+        },
+        components: {Somar}
+    }
+    
+</script>
+
+<style lang="scss">
+    .teste {
+        width: 200px;
+        height: 200px;
+        background-color: #000;
+        margin: 0 auto;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        box-sizing: border-box;
+        padding: 10px;
+        font-weight: bold;
+    }
+</style>
---
+++ src/components/Somar.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'OlhaSo',
+        data(){
+            return {
+                text: 'Clique Aqui'
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss">
+    .btn {
+        padding: 20px 10px;
+        background-color: white;
+        border: 1px solid black;
+        margin-top: 10px;
+        cursor: pointer;
+        border-radius: 2px;
+        box-sizing: border-box;
+        transition: 0.2s;
+
+        &:hover {
+            background-color: black;
+            color: white;
+            border: 1px solid white;
+        }
+    }
+</style>
